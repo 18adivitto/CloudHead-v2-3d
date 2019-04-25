fe6edf6864cdc62b43415c12b51ae5e62a8c0b3c
George Machado <gm2564@nyu.edu>
2019-04-25 12:18:23 -0400

    screen wrapping for planes is done

--- CloudHeads V.2/Assets/Code/PlaneMovement.cs
+++ CloudHeads V.2/Assets/Code/PlaneMovement.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class PlaneMovement : MonoBehaviour
 {
+    public float speed;
+
+
+    private Renderer[] _renderers;
+    private bool _isWrappingX = false;
+    private bool _isWrappingY = false;
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _renderers = GetComponentsInChildren<Renderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+
+    public void Move()
+    {
+        
+        transform.position += new Vector3(speed*Time.deltaTime, 0 , 0 );
+        ScreenWrap();
+    }
+
+    void ScreenWrap()
+    {
+        bool isVisible = CheckRenderers();
+
+        if (isVisible)
+        {
+            _isWrappingX = false;
+            _isWrappingY = false;
+            return;
+        }
+        if(_isWrappingX && _isWrappingY)
+        {
+            return;
+        }
+
+        Vector3 newPosition = transform.position;
+
+        if (newPosition.x > 1 || newPosition.x < 1)
+        {
+            newPosition.x = -newPosition.x;
+            _isWrappingX = true;
+        }
+        
+        if (newPosition.y> 1 || newPosition.y < 1)
+        {
+            newPosition.y = -newPosition.x;
+            _isWrappingY= true;
+        }
+
+        transform.position = newPosition;
+    }
+
+    bool CheckRenderers()
+    {
+        foreach (Renderer renderer in _renderers)
+        {
+            if (renderer.isVisible)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+ 
 }
