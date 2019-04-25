b766ad417a2b52abc91cfdc68ced37aaf21220ca
Andrew DIVittorio <18adivitto@gmail.com>
2019-04-25 11:14:11 -0400

    very basic 2d movement

---
+++ CloudHeads V.2/Assets/Code/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController mover;
+    Vector3 input;
+    float speed = 10;
+
+    public int playerID = 0;
+    private Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = ReInput.players.GetPlayer(playerID);
+        mover = GetComponent<CharacterController>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerInputs();
+
+        mover.Move(input * Time.deltaTime * speed);
+    }
+
+    void PlayerInputs()
+    {
+        input = new Vector3(player.GetAxis("Horizontal"),0, player.GetAxis("Vertical"));
+
+
+    }
+}
